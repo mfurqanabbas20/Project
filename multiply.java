8026ebbaeb1d9237df6c9780e4b20f0159011c8b
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2023-06-26 07:26:41 +0500

    Adding multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 32;
+        int mul = a * b;
+        System.out.print("Multiply Is: " + mul);
+    }
+}
