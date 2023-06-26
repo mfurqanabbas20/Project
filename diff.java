bc69c89be80aa985525bf834e397ee11c85b2d71
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2023-06-26 07:52:11 +0500

    Modified Diff.java

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diffc {
     public static void main(String[] args) {
         int a = 1;
         int b = 5;
-        System.out.print("The sum is: "+(a+b));
+        System.out.print("The Differenc is: "+(a-b));
     }
 }
